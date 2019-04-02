59274e30ee53a2a69000c4ed0bee8e3c9eb511a8
Mupen <45752236+Mupen@users.noreply.github.com>
2019-04-02 16:44:14 +0200

    Exception files not finished. my original was destroyed

---
+++ Week7-ExceptionHandling/src/main/java/se/lexicon/daniel/Week7_Exception/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.daniel.Week7_Exception;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
